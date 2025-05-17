a1629c95aa368b370f0ce60fad6bf619fbe4694b
Lattawit Kulanuwat <sususa003@gmail.com>
2025-05-17 18:09:20 +0700

    feat(multikeylock): add lock functions

--- src/multikeylock.rs
+++ src/multikeylock.rs
@@ -1,8 +1,18 @@
 use dashmap::DashMap;
-use std::{sync::Arc, time::Duration};
+use std::{
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
+use tokio::time::sleep;
+use tokio_util::sync::CancellationToken;
 
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(5);
 const DEFAULT_RETRY: Duration = Duration::from_millis(10);
+
+static GLOBAL_COUNTER: AtomicU64 = AtomicU64::new(0);
 
 #[derive(Debug)]
 pub struct Config {
@@ -38,6 +48,45 @@ impl MultiKeyLock {
             locks: Arc::new(config.map),
             timeout: config.timeout.unwrap_or_else(|| DEFAULT_TIMEOUT),
             retry: config.retry.unwrap_or_else(|| DEFAULT_RETRY),
+        }
+    }
+
+    pub async fn lock<K: Into<String>>(&self, key: K) -> bool {
+        self.lock_with_timeout(key, self.timeout).await
+    }
+
+    pub async fn lock_with_timeout<K: Into<String>>(&self, key: K, timeout: Duration) -> bool {
+        let cancel = CancellationToken::new();
+
+        tokio::spawn({
+            let cancel = cancel.clone();
+            async move {
+                tokio::time::sleep(timeout).await;
+                cancel.cancel();
+            }
+        });
+
+        self.lock_with_token(key, cancel).await
+    }
+
+    pub async fn lock_with_token<K: Into<String>>(
+        &self,
+        key: K,
+        cancel: CancellationToken,
+    ) -> bool {
+        let key: String = key.into();
+        let token_id = GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst);
+
+        loop {
+            let loaded = self.locks.entry(key.clone()).or_insert(token_id);
+            if *loaded == token_id {
+                return true;
+            }
+
+            tokio::select! {
+                _ = cancel.cancelled() => return false,
+                _ = sleep(self.retry) => {}
+            }
         }
     }
 }
